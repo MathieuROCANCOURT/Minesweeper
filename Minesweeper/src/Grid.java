d825494db6d084154d7d7b27b5e9ec03af8f72c5
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 11:44:27 +0200

    feat: Add displayGrid method.

--- Minesweeper/src/Grid.java
+++ Minesweeper/src/Grid.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  */
@@ -11,8 +12,36 @@ public class Grid {
 	public StateCase[][] initGrid(int nbRow, int nbColumn) {
 		StateCase[][] grid = new StateCase[nbRow][nbColumn];
 		Arrays.fill(grid, StateCase.NO_SELECT);
-		
+
 		return grid;
 	}
 
+	public void displayGrid(StateCase[][] grid, int[][] bombs) {
+		int nbLine = 1;
+		for (StateCase[] lineGrid : grid) {
+			System.out.print(nbLine + " ");
+			nbLine++;
+			for (StateCase stateCase : lineGrid) {
+				System.out.print("|");
+				
+				switch (stateCase) {
+				case NO_SELECT:
+					System.out.print(" ");
+					break;
+				case SELECT:
+					if (isBomb(bombs)) {
+						System.out.print('B');
+					} else {
+						System.out.print(nbBombAround(bombs));
+					}
+					break;
+				case POTENTIAL_BOMB:
+					System.out.print("!");
+				}
+				
+				System.out.print("|");
+			}
+			System.out.println();
+		}
+	}
 }
