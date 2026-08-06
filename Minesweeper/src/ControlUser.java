5d1e7e46eb8943f30f1a3b10a1e30869466838f6
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 13:59:56 +0200

    feat: Add revealBox method.

--- Minesweeper/src/ControlUser.java
+++ Minesweeper/src/ControlUser.java
@@ -26,14 +26,36 @@ public class ControlUser {
 
 	private static boolean correctSelect(String indexSelect, int nbRowOrColumn) {
 		int index;
-		
+
 		try {
 			index = Integer.parseInt(indexSelect);
 		} catch (Exception e) {
 			System.err.println("Vouc n'avez pas saisie un nombre.");
 			return false;
 		}
-		
+
 		return 0 <= index && index < nbRowOrColumn;
+	}
+
+	public static boolean revealBox(Scanner sc) {
+		int option = 0;
+		String optionUser;
+
+		do {
+			System.out.println("Avec cette case, voulez-vous:\n" + "[1] Révélez la case.\n"
+					+ "[2] Indiquez une potentielle BOMBE!");
+			optionUser = sc.nextLine();
+			try {
+				option = Integer.parseInt(optionUser);
+			} catch (Exception e) {
+				System.err.println("Votre saisie n'est pas un entier");
+			}
+
+			if (option <= 0 || option > 2) {
+				System.err.println("Votre saisie n'est pas un 1 ou 2.");
+			}
+		} while (option > 0 && option <= 2);
+
+		return option == 1;
 	}
 }
