2aec88b85a247ef45f3dc0fdd0d1b0e5f3d36369
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 12:54:24 +0200

    feat: Add nbBumbAround method.

--- Minesweeper/src/Grid.java
+++ Minesweeper/src/Grid.java
@@ -32,7 +32,7 @@ public class Grid {
 					if (isBomb(bombs, indexRow, indexColumn)) {
 						System.out.print('B');
 					} else {
-						System.out.print(nbBombAround(bombs));
+						System.out.print(nbBombAround(bombs, indexRow, indexColumn));
 					}
 					break;
 				case POTENTIAL_BOMB:
@@ -52,5 +52,18 @@ public class Grid {
 			}
 		}
 		return false;
+	}
+
+	private int nbBombAround(int[][] bombs, int row, int column) {
+		int countBumb = 0;
+		
+		for (int indexRow = row - 1; indexRow <= row + 1; indexRow++) {
+			for (int indexColumn = column - 1; indexColumn <= column + 1; indexColumn++) {
+				if (isBomb(bombs, indexRow, indexColumn)) {
+					countBumb++;
+				}
+			}
+		}
+		return countBumb;
 	}
 }
