c11983c45531e74466c4d2793d69616ad1123fe3
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 13:43:17 +0200

    feat: Add correctSelect method.

--- Minesweeper/src/ControlUser.java
+++ Minesweeper/src/ControlUser.java
@@ -12,15 +12,19 @@ public class ControlUser {
 			selectRow = sc.nextLine();
 		}
 
-		System.out.print("Veuillez saisir le numéro de ligne: ");
+		System.out.print("Veuillez saisir le numéro de colonne: ");
 		String selectColumn = sc.nextLine();
 
 		while (!correctSelect(selectColumn, nbColumn)) {
 			System.err.println("Saisie incorrect");
-			System.out.print("Veuillez saisir le numéro de ligne: ");
+			System.out.print("Veuillez saisir le numéro de colonne: ");
 			selectColumn = sc.nextLine();
 		}
 
 		return new int[] { Integer.parseInt(selectRow), Integer.parseInt(selectColumn) };
+	}
+
+	private static boolean correctSelect(String indexSelect, int nbRowOrColumn) {
+		return 0 <= Integer.parseInt(indexSelect) && Integer.parseInt(indexSelect) < nbRowOrColumn;
 	}
 }
