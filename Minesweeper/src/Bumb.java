495f951654fd0d4a42880e2e66719ce87eab2c86
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 13:18:14 +0200

    feat: Add createNbBumbs method.

--- Minesweeper/src/Bumb.java
+++ Minesweeper/src/Bumb.java
@@ -1,7 +1,21 @@
+import java.util.Random;
 
 /**
  * 
  */
 public class Bumb {
-
+	public int[][] createNbBumbs(int nbBumbs, int nbRows, int nbColumns) {
+		int[][] listOfBumb = new int[nbBumbs][2];
+		
+		for (int nbBumbCreate = 0; nbBumbCreate < nbBumbs; nbBumbCreate++) {
+			int[] bumbCoordinate = new int[2];
+			do {
+				Random random = new Random();
+				bumbCoordinate[0] = random.nextInt(nbRows);
+				bumbCoordinate[1] = random.nextInt(nbColumns);
+			} while (isBumb(listOfBumb, bumbCoordinate[0], bumbCoordinate[1]));
+		}
+		
+		return listOfBumb;
+	}
 }
