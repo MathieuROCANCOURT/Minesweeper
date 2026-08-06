9a9361faca72bfa7727cbe86af53984cda9f8867
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 13:40:06 +0200

    feat: Add addCoordinate method.

--- Minesweeper/src/ControlUser.java
+++ Minesweeper/src/ControlUser.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class ControlUser {
+	public static int[] addCoordinate(Scanner sc, int nbRow, int nbColumn) {
+		System.out.println("Quelle case voulez-vous visez ?");
+		System.out.print("Veuillez saisir le numéro de ligne: ");
+		String selectRow = sc.nextLine();
 
+		while (!correctSelect(selectRow, nbRow)) {
+			System.err.println("Saisie incorrect");
+			System.out.print("Veuillez saisir le numéro de ligne: ");
+			selectRow = sc.nextLine();
+		}
+
+		System.out.print("Veuillez saisir le numéro de ligne: ");
+		String selectColumn = sc.nextLine();
+
+		while (!correctSelect(selectColumn, nbColumn)) {
+			System.err.println("Saisie incorrect");
+			System.out.print("Veuillez saisir le numéro de ligne: ");
+			selectColumn = sc.nextLine();
+		}
+
+		return new int[] { Integer.parseInt(selectRow), Integer.parseInt(selectColumn) };
+	}
 }
