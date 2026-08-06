c31f5aefd6504ce01811d24922f69fdb37bba721
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 14:26:42 +0200

    fix: Resolve error generate coordinate with random.

--- Minesweeper/src/Bumb.java
+++ Minesweeper/src/Bumb.java
@@ -4,13 +4,14 @@ import java.util.Random;
  * 
  */
 public class Bumb {
-	public int[][] createNbBumbs(int nbBumbs, int nbRows, int nbColumns) {
+	private static Random random = new Random();
+	
+	public static int[][] createNbBumbs(int nbBumbs, int nbRows, int nbColumns) {
 		int[][] listOfBumb = new int[nbBumbs][2];
 		
 		for (int nbBumbCreate = 0; nbBumbCreate < nbBumbs; nbBumbCreate++) {
 			int[] bumbCoordinate = new int[2];
 			do {
-				Random random = new Random();
 				bumbCoordinate[0] = random.nextInt(nbRows);
 				bumbCoordinate[1] = random.nextInt(nbColumns);
 			} while (isBumb(listOfBumb, bumbCoordinate[0], bumbCoordinate[1]));
