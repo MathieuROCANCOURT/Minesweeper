5b6a9e901a62cffeda64656e5549d6debe9846bd
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 11:29:35 +0200

    feat: Add initGrid method.

--- Minesweeper/src/Grid.java
+++ Minesweeper/src/Grid.java
@@ -2,9 +2,17 @@
  * 
  */
 
+import java.util.Arrays;
+
 /**
  * 
  */
 public class Grid {
+	public StateCase[][] initGrid(int nbRow, int nbColumn) {
+		StateCase[][] grid = new StateCase[nbRow][nbColumn];
+		Arrays.fill(grid, StateCase.NO_SELECT);
+		
+		return grid;
+	}
 
 }
