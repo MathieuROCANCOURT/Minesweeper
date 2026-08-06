adc4c882ac53ea1def95509ac53f6a672d1f9c45
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-06 11:48:55 +0200

    feat: Add isBomb method.

--- Minesweeper/src/Grid.java
+++ Minesweeper/src/Grid.java
@@ -18,18 +18,18 @@ public class Grid {
 
 	public void displayGrid(StateCase[][] grid, int[][] bombs) {
 		int nbLine = 1;
-		for (StateCase[] lineGrid : grid) {
+		for (int indexRow = 0; indexRow < grid.length; indexRow++) {
 			System.out.print(nbLine + " ");
 			nbLine++;
-			for (StateCase stateCase : lineGrid) {
+			for (int indexColumn = 0; indexRow < grid[0].length; indexColumn++) {
 				System.out.print("|");
 				
-				switch (stateCase) {
+				switch (grid[indexRow][indexColumn]) {
 				case NO_SELECT:
 					System.out.print(" ");
 					break;
 				case SELECT:
-					if (isBomb(bombs)) {
+					if (isBomb(bombs, indexRow, indexColumn)) {
 						System.out.print('B');
 					} else {
 						System.out.print(nbBombAround(bombs));
@@ -43,5 +43,14 @@ public class Grid {
 			}
 			System.out.println();
 		}
+	}
+
+	private boolean isBomb(int[][] bombs, int row, int column) {
+		for (int[] bomb: bombs) {
+			if (bomb[0] == row && bomb[1] == column) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
