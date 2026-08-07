6a9ad04a6309ecd3cb548cd6a8d295d7e6e23bee
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-07 09:23:30 +0200

    fix: Resolve problem generate coordinate [0,0].

--- Minesweeper/src/Bumb.java
+++ Minesweeper/src/Bumb.java
@@ -1,29 +1,32 @@
-import java.util.Random;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 /**
  * 
  */
 public class Bumb {
-	private static Random random = new Random();
-	
+	private static SecureRandom random = new SecureRandom();
+
 	public static int[][] createNbBumbs(int nbBumbs, int nbRows, int nbColumns) {
 		int[][] listOfBumb = new int[nbBumbs][2];
-		
+
 		for (int nbBumbCreate = 0; nbBumbCreate < nbBumbs; nbBumbCreate++) {
 			int[] bumbCoordinate = new int[2];
+			
 			do {
 				bumbCoordinate[0] = random.nextInt(nbRows);
 				bumbCoordinate[1] = random.nextInt(nbColumns);
-			} while (isBumb(listOfBumb, bumbCoordinate[0], bumbCoordinate[1]));
-			
-			listOfBumb[nbBumbCreate] = bumbCoordinate;
+			} while (isBumb(nbBumbCreate == 0 ? new int[0][0] : Arrays.copyOfRange(listOfBumb, 0, nbBumbCreate),
+					bumbCoordinate[0], bumbCoordinate[1]));
+
+			listOfBumb[nbBumbCreate] = new int[] { bumbCoordinate[0], bumbCoordinate[1] };
 		}
-		
+
 		return listOfBumb;
 	}
 
 	public static boolean isBumb(int[][] bombs, int row, int column) {
-		for (int[] bomb: bombs) {
+		for (int[] bomb : bombs) {
 			if (bomb[0] == row && bomb[1] == column) {
 				return true;
 			}
@@ -33,7 +36,7 @@ public class Bumb {
 
 	public static int nbBumbAround(int[][] bombs, int row, int column) {
 		int countBumb = 0;
-		
+
 		for (int indexRow = row - 1; indexRow <= row + 1; indexRow++) {
 			for (int indexColumn = column - 1; indexColumn <= column + 1; indexColumn++) {
 				if (Bumb.isBumb(bombs, indexRow, indexColumn)) {
